b52c1a208f085c80cff2babf2ce372ecc2bf091f
Ayushsharma454 <ayushsharma454@gmail.com>
2023-09-25 08:58:59 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Component1 from './components/Status-card/Status';
-import Component4 from './components/userDetails/UserDetails';
 import LineChart from './components/SalesGraph/LineChart';
 import BarGraph from './components/BarGraph/BarGraph';
 import UserDetails from './components/userDetails/UserDetails';
